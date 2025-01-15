22a1a67a00742dd629f3d2f637f81ecc1dc83242
davewil <davewil1973@gmail.com>
2025-01-15 19:52:38 +0000

    Initial Commit

---
+++ lib/aoc2019_elixir.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2019Elixir do
+  @moduledoc """
+  Documentation for `Aoc2019Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2019Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2019_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2019ElixirTest do
+  use ExUnit.Case
+  doctest Aoc2019Elixir
+
+  test "greets the world" do
+    assert Aoc2019Elixir.hello() == :world
+  end
+end
